88607c548d8c3f307857b8ffcc685d0da96d7318
Matan Broner <matanbroner@gmail.com>
2021-09-07 12:03:21 -0500

    init fs

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PyBraid
+A simple implementation of <a href="https://braid.org/">Braid</a> for HTTP state synchronization in Flask
+
+## Status and Stability
+Early stage, no major work done. Still investigating the best way to inject Braid specifications into Flask application wrapper.
---
+++ init_env.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+ESC="\x1B"
+
+# Pretty prompt
+echo -e "$ESC[1;34mPyBraid Env Init$ESC[0m"
+
+# Create Python virtual environment
+python3 -m venv venv
+source venv/bin/activate
+
+# Install dependencies
+pip install -r requirements.txt
+
+# If command passed as argument, run it
+if [ "$1" ]; then
+    $@
+fi
